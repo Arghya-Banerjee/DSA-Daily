5085cc9a6e77c39b4eb5d49eb4d6f380e231034b
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-02-28 19:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (70.53%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int uniquePaths(int n, int m) {
+        int dp[102][102];
+        memset(dp, 0, sizeof(dp));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
