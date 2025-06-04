36be945a06da43929bca66e10cdd578bd6cd4fd2
Arghya Banerjee <arghya.banerjee.dev@gmail.com>
2025-06-04 10:54:23 +0530

    Time: 71 ms (55.22%) | Memory: 169.2 MB (24.63%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -3,24 +3,12 @@ public:
     string answerString(string word, int k) {
         if(k == 1) return word;
         int n = word.size();
-        char temp = 'a';
-        for(int i = 0; i < n; i++) {
-            if(word[i] > temp) {
-                temp = word[i];
-            }
-        }
-        vector<int> ind;
-        for(int i = 0; i < n; i++) {
-            if(word[i] == temp) ind.push_back(i);
-        }
         int maxLen = n - k + 1;
         string ans = "";
-        for(auto it: ind) {
+        for(int it = 0; it < n; it++) {
             string s = word.substr(it, maxLen);
             if(s > ans) ans = s;
         }
         return ans;
     }
 };
-
-// dbddac
